47d5fecd04e090ab22836059571d31d73125c491
shunsuke <shun.8000.sf@gmail.com>
2023-05-27 17:38:15 +0900

    メニューの表示にアニメーションを追加

--- components/atoms/Menu.vue
+++ components/atoms/Menu.vue
@@ -1,10 +1,12 @@
 <template>
-  <template v-if="on">
+  <Transition name="fade-mask">
     <Mask @click="handleClick" :on="on" />
-    <div class="menu" :style="getStyle">
+  </Transition>
+  <Transition name="fade-menu">
+    <div v-if="on" class="menu" :style="getStyle">
       <slot></slot>
     </div>
-  </template>
+  </Transition>
 </template>
 
 <script setup lang="ts">
@@ -48,5 +50,22 @@
     border-radius: 10px;
     background-color: var(--white);
     box-shadow: 0 0 6px 1px rgba(0, 0, 0, 0.3);
+  }
+
+  .fade-mask-enter-active,
+  .fade-mask-leave-active,
+  .fade-menu-enter-active,
+  .fade-menu-leave-active {
+    transition: opacity 0.25s ease, transform 0.25s ease;
+  }
+
+  .fade-mask-enter-from,
+  .fade-mask-leave-to {
+    opacity: 0;
+  }
+  .fade-menu-enter-from,
+  .fade-menu-leave-to {
+    opacity: 0;
+    transform: translateY(-12px);
   }
 </style>
